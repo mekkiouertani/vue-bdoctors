cd3853ada746974482a5ff92f76b9b3911e3461f
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-02-15 11:14:37 +0100

    Co-authored-by: Stefano Zidda <Trotteriniku@users.noreply.github.com> Co-authored-by: Alberto Stizzoli <albertostizzoli@users.noreply.github.com> Co-authored-by: luca-masera <luca-masera@users.noreply.github.com> updated axios Co-authored-by: SimoneNegrisoli <SimoneNegrisoli@users.noreply.github.com> Co-authored-by: Francesco Carrara <fcarrara24@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
- + ''<template>
+<template>
   <router-view>
   </router-view>
 </template>
@@ -17,23 +17,36 @@ export default {
     }
   },
   methods: {
-    getAllDatas(){
-      axios.get(store.apiUrl + '/accounts').then((res) =>{
+    getAllDatas() {
+      axios.get(store.apiUrl + '/accounts').then((res) => {
         this.store.allDoctors = this.store.allDoctors.concat(res.data.results);
-        console.log('pippo',this.store.allDoctors);
-        
+        console.log('pippo', this.store.allDoctors);
       })
+
       //getting alla specializations
-      axios.get(store.apiUrl + '/specializations').then((res) =>{
+      axios.get(store.apiUrl + '/specializations').then((res) => {
         this.store.allSpecializations = this.store.allSpecializations.concat(res.data.results);
-        console.log('specializations',this.store.allSpecializations);
-        
+        console.log('specializations', this.store.allSpecializations);
       })
+    },
+    //funzione per filtrare per specializations
+    filteredSpecializations() {
+      if (this.store.selectedSpecializations) {
+        axios.get(`${this.store.apiUrl}/accounts`, { params: { specialization: this.store.selectedSpecializations } }).then((res) => {
+          this.store.allDoctors = res.data.results;
+          console.log(`filter`, res.data.results);
+        }).catch((err) => {
+          console.log(err);
+        });
+      } else {
+        this.getAllDatas();
+      }
     }
   },
   mounted() {
-    this.getAllDatas()
-    
+    this.getAllDatas();
+    this.filteredSpecializations();
+
   },
 }
 </script>
--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,9 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiUrl: "http://localhost:8000/api",
-    allDoctors: [],
-    allSpecializations: []
+  apiUrl: "http://localhost:8000/api",
+  allDoctors: [],
+  allSpecializations: [],
+  fileredSpecialzations: [],
+  selectedSpecializations: 1,
 });
