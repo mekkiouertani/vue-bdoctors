9ce4f4db15e1cc1dc7ce93f2fd7362284403f703
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-02-19 16:04:46 +0100

    cv form fix
    Co-authored-by: Francesco Carrara <fcarrara24@users.noreply.github.com>
    Co-authored-by: Alberto Stizzoli <albertostizzoli@users.noreply.github.com>
    Co-authored-by: luca-masera <luca-masera@users.noreply.github.com>

--- src/pages/DetailDoctor.vue
+++ src/pages/DetailDoctor.vue
@@ -2,32 +2,34 @@
   <div class="detail-doctor">
     <TopBar />
     <header>
-      <img v-if="doctor?.image" :src="`${store.basePathImage}${doctor.image}`" class="doctor-image"
+      <img v-if="doctor.image" :src="`${store.basePathImage}${doctor.image}`" class="doctor-image"
         alt="Immagine del dottore">
     </header>
     <div class="container">
       <div class="row">
         <!-- Colonna Informazioni -->
-        <div class="col-12 col-md-6 pt-3 pb-3 ">
-          <div class="card">
-            <div class="card-body m-3 ">
-              <h3 class="title text-center">INFORMAZIONI</h3>
-              <p class="p-2"><strong>Nome:</strong> {{ doctor.user.name }}</p>
-              <p class="p-2"><strong>Cognome:</strong> {{ doctor.user.surname }}</p>
+        <div class="col-12 pt-3 pb-3" :class="doctor.cv ? 'col-md-6' : ''">
+          <div class="card h-100"
+            :class="!doctor.cv ? 'd-flex flex-row justify-content-between align-contents-start' : ''">
+            <div class="card-body mt-2">
+              <h3 class="title ">INFORMAZIONI</h3>
+              <p class="p-1"><strong>Nome:</strong> {{ doctor.user.name }}</p>
+              <p class="p-1"><strong>Cognome:</strong> {{ doctor.user.surname }}</p>
             </div>
             <!-- Colonna Specializzazione -->
-            <div class="card-body m-3">
-              <h3 class="title text-center">SPECIALIZZAZIONE</h3>
+            <div class="card-body mt-2">
+              <h3 class="title ">SPECIALIZZAZIONE</h3>
               <div class="p-1 d-flex ">
                 <strong>Specializzazione:</strong>
                 <ul>
-                  <li v-for="specialization in doctor.specializations">{{ specialization.name }}</li>
+                  <li class="list" v-for="specialization in doctor.specializations">{{ specialization.name }}
+                  </li>
                 </ul>
               </div>
             </div>
             <!-- Colonna Contatti -->
-            <div class="card-body m-3">
-              <h3 class="title text-center">CONTATTI</h3>
+            <div class="card-body mt-2">
+              <h3 class="title ">CONTATTI</h3>
               <p class="p-1"><strong>Indirizzo:</strong> {{ doctor.address }}</p>
               <p class="p-1"><strong>Email:</strong> {{ doctor.user.email }}</p>
               <p class="p-1"><strong>Numero di telefono:</strong> {{ doctor.phone }}</p>
@@ -35,12 +37,11 @@
           </div>
         </div>
         <!-- Colonna CV -->
-        <div class="col-12 col-md-6 pt-3 pb-3">
+        <div v-if="doctor.cv" class="col-12 col-md-6 pt-3 pb-3">
           <div class="card h-100  ">
             <div class="card-body ">
               <h3 class="title text-center">Curriculum Vitae</h3>
-              <embed class="w-100  " :src="`${store.basePathCV}${doctor.cv}`" type="application/pdf"
-                style="height: 680px;">
+              <iframe class="w-100  " :src="`${doctor.cv}`" type="application/pdf" style="height: 680px;"></iframe>
             </div>
           </div>
         </div>
