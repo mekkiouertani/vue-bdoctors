997eba2f465d37fcefde1658c4e8e4fc5c047418
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-02-23 12:17:22 +0100

    not showing sponsored doctors where there are none

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,15 +1,21 @@
 <template>
     <TopBar />
     <HeroSection />
-    <SponsoredDoctors />
+    <SponsoredDoctors v-if="this.store.allDoctors.length > 0"/>
 </template>
 
 <script>
+import {store} from '../data/store.js'
 import TopBar from './../components/TopBar.vue';
 import HeroSection from './../components/HeroSection.vue';
 import SponsoredDoctors from './../components/SponsoredDoctors.vue';
 export default {
     components: { TopBar, SponsoredDoctors, HeroSection },
+    data(){
+        return {
+            store,
+        }
+    },
     name: "AppHome",
 }
 </script>
