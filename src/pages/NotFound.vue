f9e723e282d1268f647df52f8251bc68f93ae7a6
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2024-02-25 17:06:13 +0100

    added notfound router

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,84 @@
+<template>
+    <section class="page_404 d-flex justify-content-center ">
+        <div class="">
+            <div class="row">
+                <div class="col-sm-12 ">
+                    <div class="col-sm-10 col-sm-offset-1  text-center">
+                        <div class="four_zero_four_bg">
+                            <h1 class="text-center ">404</h1>
+
+
+                        </div>
+
+                        <div class="contant_box_404">
+                            <h3 class="h2">
+                                Qualcosa è andato storto
+                            </h3>
+
+                            <p>la pagina che stavi cercando non è disponibile!</p>
+
+                            <router-link :to="{ name: 'home' }" class="btn btn-primary" active-class="active">
+                                Home
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+/*======================
+    404 page
+=======================*/
+
+section {
+    background-color: white;
+    height: 100vh;
+
+}
+
+.page_404 {
+    padding: 40px 0;
+    background: #fff;
+}
+
+.page_404 img {
+    width: 100%;
+}
+
+.four_zero_four_bg {
+
+    background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+    height: 400px;
+    background-position: center;
+}
+
+
+.four_zero_four_bg h1 {
+    font-size: 80px;
+}
+
+.four_zero_four_bg h3 {
+    font-size: 80px;
+}
+
+.link_404 {
+    color: #fff !important;
+    padding: 10px 20px;
+    background: #39ac31;
+    margin: 20px 0;
+    display: inline-block;
+}
+
+.contant_box_404 {
+    margin-top: -50px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,31 +2,44 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "../src/pages/AppHome.vue";
 import AdvancedSearch from "../src/pages/AdvancedSearch.vue";
 import DetailDoctor from "../src/pages/DetailDoctor.vue";
+import NotFound from "../src/pages/NotFound.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: AppHome,
+    meta: { title: "BDoctors" },
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: AdvancedSearch,
+    meta: { title: "BDoctors - Cerca" },
+  },
+  {
+    path: "/detail-doctor/:id",
+    name: "detail-doctor",
+    component: DetailDoctor,
+    meta: { title: "BDoctors - Profilo Medico" },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "not-found",
+    component: NotFound,
+    meta: { title: "BDoctors - Error 404" },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: AppHome,
-    },
-    {
-      path: "/search",
-      name: "search",
-      component: AdvancedSearch,
-    },
-    {
-      path: "/detail-doctor/:id",
-      name: "detail-doctor",
-      component: DetailDoctor,
-    },
-    /*  {
-      path: "/:pathMatch(.*)*",
-      name: "not-found",
-      component: NotFound,
-    }, */
-  ],
+  routes,
+});
+
+// Watcher globale per cambiamenti di route
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || "Titolo Predefinito"; // Imposta il titolo della pagina
+  next();
 });
 
 export default router;
